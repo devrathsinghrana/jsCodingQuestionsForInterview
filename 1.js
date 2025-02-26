1ea5b6360efa31400a6f53b97434fe4d829f1e8f
devrathrana <devrath.rana@bold.com>
2025-02-26 14:16:51 +0530

    coding 1

---
+++ 1.js
@@ -0,0 +1,16 @@
+const students = [
+    { registerNo: 101, subject: 'Maths', score: 60 },
+    { registerNo: 102, subject: 'Maths', score: 70 },
+    { registerNo: 101, subject: 'Science', score: 50 }
+ ];
+ 
+ const result = Object.values(
+     students.reduce((acc, { registerNo, score }) => {
+         acc[registerNo] = acc[registerNo] || { registerNo, score: 0 };
+         acc[registerNo].score += score;
+         return acc;
+     }, {})
+ );
+ 
+ console.log(result);
+ 
